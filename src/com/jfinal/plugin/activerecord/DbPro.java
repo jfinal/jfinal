8606cd82eff06fed937315478255fad261b50c6f
SnailKnows <cannel_2020@163.com>
2015-04-14 22:13:53 +0800

    Update DbPro.java

--- src/com/jfinal/plugin/activerecord/DbPro.java
+++ src/com/jfinal/plugin/activerecord/DbPro.java
@@ -647,9 +647,6 @@ public class DbPro {
 			else
 				conn.rollback();
 			return result;
-		} catch (NestedTransactionHelpException e) {
-			if (conn != null) try {conn.rollback();} catch (Exception e1) {e1.printStackTrace();}
-			return false;
 		} catch (Throwable t) {
 			if (conn != null) try {conn.rollback();} catch (Exception e1) {e1.printStackTrace();}
 			throw t instanceof RuntimeException ? (RuntimeException)t : new ActiveRecordException(t);
