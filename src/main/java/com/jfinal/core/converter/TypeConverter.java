c6fb471ed5410414d296d8dd8756d13c594e3248
James <jfinal@126.com>
2017-06-20 23:06:27 +0800

    修改注释

--- src/main/java/com/jfinal/core/converter/TypeConverter.java
+++ src/main/java/com/jfinal/core/converter/TypeConverter.java
@@ -33,8 +33,14 @@ import com.jfinal.core.converter.Converters.TimeConverter;
 import com.jfinal.core.converter.Converters.TimestampConverter;
 
 /**
- *  用户可以实现自己的类型转换器，并进行注册。
- *  TypeConverter.me().regist(Integer.class, new IntegerConverter());
+ * TypeConverter 用于将客户端请求的 String 类型数据转换成指定的数据类型
+ * 
+ * 可以通过实现 IConverter 定制自己的类型转换规则:
+ * 1：实现 IConverter 接口，如 MyDateConverter
+ * 2：注册：TypeConverter.me().regist(Date.class, new MyDateConverter());
+ * 
+ * 注意：后注册的 Converter 会直接覆盖掉先注册的，因此可以通过覆盖的方式取代 jfinal
+ *      官方的 Converter 实现类，达成定制化的需求
  * 
  * 
  * test for all types of mysql
@@ -43,11 +49,13 @@ import com.jfinal.core.converter.Converters.TimestampConverter;
  * 1: 表单中的域,就算不输入任何内容,也会传过来 "", 也即永远不可能为 null.
  * 2: 如果输入空格也会提交上来
  * 3: 需要考 model中的 string属性,在传过来 "" 时是该转成 null还是不该转换,
- *    我想, 因为用户没有输入那么肯定是 null, 而不该是 ""
+ *    因为用户没有输入那么肯定是 null, 而不该是 ""
+ * 4： 返回 null 还可以在数据表字段约束为不允许 null 值时，避免失误清除数据，
+ *    "" 值对于数据表字段来说是合法的，容易失误清掉原有内容
  * 
  * 注意: 1:当type参数不为String.class, 且参数s为空串blank的情况,
  *       此情况下转换结果为 null, 而不应该抛出异常
- *      2:调用者需要对被转换数据做 null 判断，参见 ModelInjector 的两处调用
+ *      2:调用者需要对被转换数据做 null 判断，参见 Injector 的两处调用
  */
 public class TypeConverter {
 	
@@ -82,6 +90,12 @@ public class TypeConverter {
 		converterMap.put(type, converter);
 	}
 	
+	/**
+	 * 将 String 数据转换为指定的类型
+	 * @param type 需要转换成为的数据类型
+	 * @param s 被转换的 String 类型数据，注意： s 参数不接受 null 值，否则会抛出异常
+	 * @return 转换成功的数据
+	 */
 	public final Object convert(Class<?> type, String s) throws ParseException {
 		// mysql type: varchar, char, enum, set, text, tinytext, mediumtext, longtext
 		if (type == String.class) {
