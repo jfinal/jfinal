1f8ad09c19e86235c7df11f8c1f6dcbf4844cf4b
玛雅牛 <myaniu@gmail.com>
2017-06-22 18:14:49 +0800

    采用扁平化接口，只提供一种与微信兼容的实现

--- src/main/java/com/jfinal/kit/AesKit.java
+++ src/main/java/com/jfinal/kit/AesKit.java
@@ -1,141 +1,114 @@
-/**
- * Copyright (c) 2011-2017, 玛雅牛 (myaniu AT gmail dot com).
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.jfinal.kit;
 
 import java.nio.charset.Charset;
-import java.security.SecureRandom;
+import java.util.Arrays;
 
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
+/**
+ * 完全兼容微信所使用的AES加密方式。
+ * aes的key必须是256byte长（比如32个字符），可以使用AesKit.genAesKey()来生成一组key
+ * @ClassName: AesKit  
+ */
 public class AesKit {
-	private AesKit() {
-	}
-	public static String genRandomPassword(){
+	private static final Charset UTF_8 = Charset.forName("UTF-8");
+	
+	private AesKit(){}
+	
+	public static String genAesKey(){
 		return HashKit.generateSalt(32);
 	}
-	public static Encrypter getAes128Encrypter(String password){
-		return new Encrypter(128,password);
-	}
-	public static Encrypter getAes192Encrypter(String password){
-		return new Encrypter(192,password);
-	}
-	public static Encrypter getAes256Encrypter(String password){
-		return new Encrypter(256,password);
-	}
-	public static Decrypter getAes128Decrypter(String password){
-		return new Decrypter(128,password);
-	}
-	public static Decrypter getAes192Decrypter(String password){
-		return new Decrypter(192,password);
-	}
-	public static Decrypter getAes256Decrypter(String password){
-		return new Decrypter(256,password);
+	
+	public static byte[] encrypt(byte[] content, String aesTextKey) {
+		return encrypt(content, aesTextKey.getBytes(UTF_8));
 	}
 	
-	static abstract class Aes{
-		protected static final Charset UTF_8 = Charset.forName("UTF-8");
-		protected final int keyLen;
-		protected final String password;
-		Aes(int keyLen,String password){
-			this.keyLen = keyLen;
-			this.password = password;
-		}
+	public static byte[] encrypt(String content, String aesTextKey) {
+		return encrypt(content.getBytes(UTF_8), aesTextKey.getBytes(UTF_8));
 	}
-	public static class Encrypter extends Aes{
-		Encrypter(int keyLen, String password){
-			super(keyLen, password);
-		}
-		public byte[] encrypt(byte[] content) {
-			if(null == content || content.length == 0){
-				return content;
+	
+	public static byte[] encrypt(String content, String charsetName, String aesTextKey){
+		return encrypt(content.getBytes(Charset.forName(charsetName)), aesTextKey.getBytes(UTF_8));
+	}
+	
+	public static byte[] decrypt(byte[] content, String aesTextKey) {
+		return decrypt(content, aesTextKey.getBytes(UTF_8));
+	}
+	
+	public static String decryptToStr(byte[] content, String aesTextKey) {
+		return new String(decrypt(content, aesTextKey.getBytes(UTF_8)), UTF_8);
+	}
+	
+	public static String decryptToStr(byte[] content, String aesTextKey, String charsetName) {
+		return new String(decrypt(content, aesTextKey.getBytes(UTF_8)), Charset.forName(charsetName));
+	}
+	
+	public static byte[] encrypt(byte[] content, byte[] aesKey){
+		try {
+			if(aesKey.length != 32){
+				throw new RuntimeException("IllegalAesKey, aesKey's length must be 32");
 			}
-			return encrypt(keyLen,content, password);
-		}
-		public byte[] encrypt(String content) {
-			if(null == content){
-				return null;
-			}
-			return encrypt(keyLen,content.getBytes(UTF_8), password);
-		}
-		public byte[] encrypt(String content, String charsetName){
-			if(null == content){
-				return null;
-			}
-			return encrypt(keyLen, content.getBytes(Charset.forName(charsetName)), password);
-		}
-		private static byte[] encrypt(int keyLen, byte[] content, String password){
-			try {
-				KeyGenerator kgen = KeyGenerator.getInstance("AES");
-				SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG" );
-	            secureRandom.setSeed(password.getBytes());
-				kgen.init(keyLen, secureRandom);
-				SecretKey secretKey = kgen.generateKey();
-				byte[] enCodeFormat = secretKey.getEncoded();
-				SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
-				Cipher cipher = Cipher.getInstance("AES");// 创建密码器
-				cipher.init(Cipher.ENCRYPT_MODE, key);// 初始化
-				byte[] result = cipher.doFinal(content);
-				return result; // 加密
-			} catch (Exception e) {
-				throw new RuntimeException(e.getMessage(),e);
-			}
+			Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+            SecretKeySpec keySpec = new SecretKeySpec(aesKey, "AES");
+            IvParameterSpec iv = new IvParameterSpec(aesKey, 0, 16);
+            cipher.init(Cipher.ENCRYPT_MODE, keySpec, iv);
+            return cipher.doFinal(PKCS7Encoder.encode(content));
+		} catch (Exception e) {
+			throw new RuntimeException(e.getMessage(),e);
 		}
 	}
 
-	public static class Decrypter extends Aes{
-		Decrypter(int keyLen, String password){
-			super(keyLen,password);
-		}
-		public byte[] decrypt(byte[] content) {
-			if(null == content || content.length == 0){
-				return content;
+	public static byte[] decrypt(byte[] encrypted, byte[] aesKey){
+		try {
+			if(aesKey.length != 32){
+				throw new RuntimeException("IllegalAesKey, aesKey's length must be 32");
 			}
-			return decrypt(keyLen, content, password);
+			Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+            SecretKeySpec key_spec = new SecretKeySpec(aesKey, "AES");
+            IvParameterSpec iv = new IvParameterSpec(Arrays.copyOfRange(aesKey, 0, 16));
+            cipher.init(Cipher.DECRYPT_MODE, key_spec, iv);
+            return PKCS7Encoder.decode(cipher.doFinal(encrypted));
+		} catch (Exception e) {
+			throw new RuntimeException(e.getMessage(),e);
 		}
-		public String decryptToStr(byte[] content) {
-			if(null == content || content.length == 0){
-				return "";
-			}
-			return new String(decrypt(content), UTF_8);
-		}
-		public String decryptToStr(byte[] content,String charsetName) {
-			if(null == content || content.length == 0){
-				return "";
-			}
-			return new String(decrypt(content), Charset.forName(charsetName));
-		}
-		private static byte[] decrypt(int keyLen, byte[] content, String password){
-			try {
-				KeyGenerator kgen = KeyGenerator.getInstance("AES");
-				SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG" );
-	            secureRandom.setSeed(password.getBytes());
-				kgen.init(keyLen, secureRandom);
-				SecretKey secretKey = kgen.generateKey();
-				byte[] enCodeFormat = secretKey.getEncoded();
-				SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
-				Cipher cipher = Cipher.getInstance("AES");// 创建密码器
-				cipher.init(Cipher.DECRYPT_MODE, key);// 初始化
-				byte[] result = cipher.doFinal(content);
-				return result; // 加密
-			} catch (Exception e) {
-				throw new RuntimeException(e.getMessage(),e);
-			}
-		}
+	}
+	/**
+	 * 提供基于PKCS7算法的加解密接口.
+	 */
+	static class PKCS7Encoder {
+	    static int BLOCK_SIZE = 32;
+	    
+	    static byte[] encode(byte[] src) {
+	    	int count = src.length;
+	        // 计算需要填充的位数
+	        int amountToPad = BLOCK_SIZE - (count % BLOCK_SIZE);
+	        if (amountToPad == 0) {
+	            amountToPad = BLOCK_SIZE;
+	        }
+	        // 获得补位所用的字符
+	        byte pad = (byte) (amountToPad & 0xFF);
+	        byte[] pads = new byte[amountToPad];
+	        for (int index = 0; index < amountToPad; index++) {
+	        	pads[index] = pad;
+	        }
+	        int length = count + amountToPad;
+	        byte[] dest = new byte[length];
+	        System.arraycopy(src, 0, dest, 0, count);
+	        System.arraycopy(pads, 0, dest, count, amountToPad);
+	        return dest;
+	    }
+
+	    static byte[] decode(byte[] decrypted) {
+	        int pad = (int) decrypted[decrypted.length - 1];
+	        if (pad < 1 || pad > 32) {
+	            pad = 0;
+	        }
+	        if(pad > 0){
+	        	return Arrays.copyOfRange(decrypted, 0, decrypted.length - pad);
+	        }
+	        return decrypted;
+	    }
 	}
 }
