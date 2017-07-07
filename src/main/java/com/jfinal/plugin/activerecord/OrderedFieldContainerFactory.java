e9c3e192091a1c4fc579e2188029984cc1db0baf
James <jfinal@126.com>
2017-07-07 21:58:01 +0800

    jfinal 3.2 release ^_^

--- src/main/java/com/jfinal/plugin/activerecord/OrderedFieldContainerFactory.java
+++ src/main/java/com/jfinal/plugin/activerecord/OrderedFieldContainerFactory.java
@@ -21,6 +21,15 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * 用于支持查询出来的字段次序与 select a, b, c... 的次序一致
+ * 
+ * 通常用于查询类系统，字段是不确定的，字段显示的次序要与 select
+ * 字句保持一致
+ * 
+ * 用法：
+ * arp.setContainerFactory(new OrderedFieldContainerFactory())
+ */
 @SuppressWarnings({"rawtypes", "unchecked"})
 public class OrderedFieldContainerFactory implements IContainerFactory {
 	
