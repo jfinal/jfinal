9cbd27b245c366f3d75a704492376487b40da977
James <jfinal@126.com>
2018-01-26 23:49:16 +0800

    jfinal 3.4

--- src/main/java/com/jfinal/plugin/activerecord/TableBuilder.java
+++ src/main/java/com/jfinal/plugin/activerecord/TableBuilder.java
@@ -99,7 +99,7 @@ class TableBuilder {
 					table.setColumnType(colName, java.sql.Date.class);
 				}
 				// 支持 PostgreSql 的 jsonb json
-				else if (type == Types.JAVA_OBJECT) {
+				else if (type == Types.OTHER) {
 					table.setColumnType(colName, Object.class);
 				}
 				else {
