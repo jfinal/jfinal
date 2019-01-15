b122090dc0255c854e2e69a690793e745413e423
James <jfinal@126.com>
2019-01-15 21:55:05 +0800

    jfinal 3.6

--- src/main/java/com/jfinal/template/EngineConfig.java
+++ src/main/java/com/jfinal/template/EngineConfig.java
@@ -83,7 +83,7 @@ public class EngineConfig {
 	 * Add shared function with file
 	 */
 	public void addSharedFunction(String fileName) {
-		fileName = fileName.replaceAll("\\\\", "/");
+		fileName = fileName.replace("\\", "/");
 		// FileSource fileSource = new FileSource(baseTemplatePath, fileName, encoding);
 		ISource source = sourceFactory.getSource(baseTemplatePath, fileName, encoding);
 		doAddSharedFunction(source, fileName);
@@ -262,7 +262,7 @@ public class EngineConfig {
 			throw new IllegalArgumentException("baseTemplatePath can not be blank");
 		}
 		baseTemplatePath = baseTemplatePath.trim();
-		baseTemplatePath = baseTemplatePath.replaceAll("\\\\", "/");
+		baseTemplatePath = baseTemplatePath.replace("\\", "/");
 		if (baseTemplatePath.length() > 1) {
 			if (baseTemplatePath.endsWith("/") || baseTemplatePath.endsWith("\\")) {
 				baseTemplatePath = baseTemplatePath.substring(0, baseTemplatePath.length() - 1);
