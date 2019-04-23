7b97b3ad39f8a232555efcfc36190e0c6da7b99e
James <jfinal@126.com>
2019-04-23 20:23:24 +0800

    jfinal 4.0

--- src/main/java/com/jfinal/core/Const.java
+++ src/main/java/com/jfinal/core/Const.java
@@ -23,7 +23,7 @@ import com.jfinal.render.ViewType;
  */
 public interface Const {
 	
-	String JFINAL_VERSION = "3.9";
+	String JFINAL_VERSION = "4.0";
 	
 	ViewType DEFAULT_VIEW_TYPE = ViewType.JFINAL_TEMPLATE;
 	
