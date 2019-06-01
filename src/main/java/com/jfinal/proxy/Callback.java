cd1ca572aa7dd7670954b9f67d56b5fcabeff75c
James <jfinal@126.com>
2019-06-01 23:56:37 +0800

    jfinal 4.2

--- src/main/java/com/jfinal/proxy/Callback.java
+++ src/main/java/com/jfinal/proxy/Callback.java
@@ -21,7 +21,7 @@ package com.jfinal.proxy;
  */
 @FunctionalInterface
 public interface Callback {
-	public Object call(Object[] args) throws Exception;
+	public Object call(Object[] args) throws Throwable;
 }
 
 
