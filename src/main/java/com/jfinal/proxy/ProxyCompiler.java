854097516ee946bc5f5af7cabe612c30fcc4200d
James <jfinal@126.com>
2019-11-30 14:28:24 +0800

    jfinal 4.8

--- src/main/java/com/jfinal/proxy/ProxyCompiler.java
+++ src/main/java/com/jfinal/proxy/ProxyCompiler.java
@@ -128,7 +128,8 @@ public class ProxyCompiler {
 	public void compile(ProxyClass proxyClass) {
 		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
 		if (compiler == null) {
-			throw new RuntimeException("Can not get javax.tools.JavaCompiler, check whether \"tools.jar\" is in the environment variable CLASSPATH");
+			throw new RuntimeException("Can not get javax.tools.JavaCompiler, check whether \"tools.jar\" is in the environment variable CLASSPATH \n" +
+										"Visit https://www.jfinal.com/doc/4-8 for details \n");
 		}
 		
 		DiagnosticCollector<JavaFileObject> collector = new DiagnosticCollector<>();
