555ac0d00707a3ff5e335abec4329e7601ed69c6
James <jfinal@126.com>
2020-04-11 16:21:41 +0800

    cos upgrade to 2020.4

--- src/main/java/com/jfinal/upload/ExceededSizeException.java
+++ src/main/java/com/jfinal/upload/ExceededSizeException.java
@@ -6,12 +6,16 @@ package com.jfinal.upload;
  * com.oreilly.servlet.multipart.MultipartParser 中会抛出以下异常
  * throw new ExceededSizeException("Posted content length of " + length + " exceeds limit of " + maxSize);
  */
-public class ExceededSizeException extends RuntimeException {
+public class ExceededSizeException extends com.oreilly.servlet.multipart.ExceededSizeException {
 	
 	private static final long serialVersionUID = -3493615798872340918L;
 	
-	ExceededSizeException(Throwable t) {
+	public ExceededSizeException(Throwable t) {
 		super(t);
+	}
+	
+	public ExceededSizeException(String s) {
+		super(s);
 	}
 }
 
