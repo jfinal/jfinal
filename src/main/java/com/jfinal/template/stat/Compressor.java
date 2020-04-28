1fcb5c801c89d714789f971ee6eb8853a66f102a
James <jfinal@126.com>
2020-04-29 00:59:07 +0800

    调整左右压缩次序

--- src/main/java/com/jfinal/template/stat/Compressor.java
+++ src/main/java/com/jfinal/template/stat/Compressor.java
@@ -59,16 +59,16 @@ public class Compressor {
 	 * @param result 存放压缩结果
 	 */
 	protected void compressLine(StringBuilder content, int start, int end, boolean compressLeft, StringBuilder result) {
-		// 压缩右侧空白
-		while (end >= start && content.charAt(end) <= ' ') {
-			end--;
-		}
-		
 		// 压缩左侧空白
 		if (compressLeft) {
 			while (start < end && content.charAt(start) <= ' ') {
 				start++;
 			}
+		}
+		
+		// 压缩右侧空白
+		while (end >= start && content.charAt(end) <= ' ') {
+			end--;
 		}
 		
 		if (start <= end) {
