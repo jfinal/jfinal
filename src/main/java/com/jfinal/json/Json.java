ca475327da2a678be6dcd925857c4f41f73c7eef
James <jfinal@126.com>
2020-05-10 11:58:30 +0800

    defaultDatePattern、datePattern 可配置为 null，支持输出 long 型时间戳

--- src/main/java/com/jfinal/json/Json.java
+++ src/main/java/com/jfinal/json/Json.java
@@ -16,7 +16,7 @@
 
 package com.jfinal.json;
 
-import com.jfinal.kit.StrKit;
+import java.util.Objects;
 
 /**
  * json string 与 object 互转抽象
@@ -38,23 +38,15 @@ public abstract class Json {
 	protected String datePattern = null;
 	
 	static void setDefaultJsonFactory(IJsonFactory defaultJsonFactory) {
-		if (defaultJsonFactory == null) {
-			throw new IllegalArgumentException("defaultJsonFactory can not be null.");
-		}
+		Objects.requireNonNull(defaultJsonFactory, "defaultJsonFactory can not be null");
 		Json.defaultJsonFactory = defaultJsonFactory;
 	}
 	
 	static void setDefaultDatePattern(String defaultDatePattern) {
-		if (StrKit.isBlank(defaultDatePattern)) {
-			throw new IllegalArgumentException("defaultDatePattern can not be blank.");
-		}
 		Json.defaultDatePattern = defaultDatePattern;
 	}
 	
 	public Json setDatePattern(String datePattern) {
-		if (StrKit.isBlank(datePattern)) {
-			throw new IllegalArgumentException("datePattern can not be blank.");
-		}
 		this.datePattern = datePattern;
 		return this;
 	}
