5826d5a3d2da934ebda6888a7f6edd6e76da96cf
James <jfinal@126.com>
2020-05-28 21:26:26 +0800

    禁用 fastjson 的 autoType

--- src/main/java/com/jfinal/json/FastJson.java
+++ src/main/java/com/jfinal/json/FastJson.java
@@ -17,6 +17,7 @@
 package com.jfinal.json;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.parser.ParserConfig;
 import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.jfinal.plugin.activerecord.Record;
@@ -29,6 +30,9 @@ public class FastJson extends Json {
 	static {
 		// 支持序列化 ActiveRecord 的 Record 类型
 		SerializeConfig.getGlobalInstance().put(Record.class, new FastJsonRecordSerializer());
+		
+		// 完全禁用 autoType，提升安全性
+		ParserConfig.getGlobalInstance().setSafeMode(true);
 	}
 	
 	public static FastJson getJson() {
