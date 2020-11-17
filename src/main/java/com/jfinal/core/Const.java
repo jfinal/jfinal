38145ad8be4f01feaf12d6c90ab4bdbb12d361a6
James <jfinal@126.com>
2020-11-17 14:35:15 +0800

    版本号改为 4.9.04

--- src/main/java/com/jfinal/core/Const.java
+++ src/main/java/com/jfinal/core/Const.java
@@ -23,7 +23,7 @@ import com.jfinal.render.ViewType;
  */
 public interface Const {
 	
-	String JFINAL_VERSION = "4.9.03";
+	String JFINAL_VERSION = "4.9.04";
 	
 	ViewType DEFAULT_VIEW_TYPE = ViewType.JFINAL_TEMPLATE;
 	
