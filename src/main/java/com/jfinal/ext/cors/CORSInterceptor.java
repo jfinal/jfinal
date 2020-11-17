0c6051778b5441a8b1e3f092be9c87a5b4cec411
James <jfinal@126.com>
2020-11-17 14:33:36 +0800

    改注释

--- src/main/java/com/jfinal/ext/cors/CORSInterceptor.java
+++ src/main/java/com/jfinal/ext/cors/CORSInterceptor.java
@@ -25,7 +25,7 @@ import javax.servlet.http.HttpServletResponse;
  * @author Michael Yang 杨福海 （fuhai999@gmail.com）
  * 
  * 使用方法:
- * 1、在 JFInalConfig 的 在 configInterceptor(Interceptors me) 中添加全局拦截器（也可以不是全局拦截器）：me.add(new CORSIntercetpro());
+ * 1、在 JFInalConfig 的 在 configInterceptor(Interceptors me) 中添加全局拦截器（也可以不是全局拦截器）：me.add(new CORSInterceptor());
  * 2、在需要支持跨域的 Action 方法中添加 @EnableCORS
  * 
  * PS：
