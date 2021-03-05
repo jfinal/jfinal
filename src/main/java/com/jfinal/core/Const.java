cbc669842c10db360704470961f912c0f5755406
James <jfinal@126.com>
2021-03-05 23:27:13 +0800

    修改版本号

--- src/main/java/com/jfinal/core/Const.java
+++ src/main/java/com/jfinal/core/Const.java
@@ -23,7 +23,7 @@ import com.jfinal.render.ViewType;
  */
 public interface Const {
 	
-	String JFINAL_VERSION = "4.9.08";
+	String JFINAL_VERSION = "4.9.09";
 	
 	ViewType DEFAULT_VIEW_TYPE = ViewType.JFINAL_TEMPLATE;
 	
