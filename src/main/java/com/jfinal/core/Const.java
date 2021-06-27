f0ca8e5551a95ce65952a86dc06669bfe96d6608
James <jfinal@126.com>
2021-06-28 00:56:13 +0800

    4.9.14 release ^_^

--- src/main/java/com/jfinal/core/Const.java
+++ src/main/java/com/jfinal/core/Const.java
@@ -23,7 +23,7 @@ import com.jfinal.render.ViewType;
  */
 public interface Const {
 	
-	String JFINAL_VERSION = "4.9.13";
+	String JFINAL_VERSION = "4.9.14";
 	
 	ViewType DEFAULT_VIEW_TYPE = ViewType.JFINAL_TEMPLATE;
 	
