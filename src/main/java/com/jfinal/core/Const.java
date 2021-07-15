07e3134aab39803257279e874207e7e5d755192e
James <jfinal@126.com>
2021-07-16 01:14:28 +0800

    版本号改为 4.9.16

--- src/main/java/com/jfinal/core/Const.java
+++ src/main/java/com/jfinal/core/Const.java
@@ -23,7 +23,7 @@ import com.jfinal.render.ViewType;
  */
 public interface Const {
 	
-	String JFINAL_VERSION = "4.9.15";
+	String JFINAL_VERSION = "4.9.16";
 	
 	ViewType DEFAULT_VIEW_TYPE = ViewType.JFINAL_TEMPLATE;
 	
