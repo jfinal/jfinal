e7e023238015ef6569e4f9de30899fe45d4f8ae3
James <jfinal@126.com>
2022-03-21 21:41:34 +0800

    构造方法内触发 fastjson 配置代码

--- src/main/java/com/jfinal/json/FastJsonFactory.java
+++ src/main/java/com/jfinal/json/FastJsonFactory.java
@@ -26,6 +26,11 @@ public class FastJsonFactory implements IJsonFactory {
 	
 	private static final FastJsonFactory me = new FastJsonFactory();
 	
+	public FastJsonFactory() {
+		// 尽早触发 fastjson 的配置代码
+		new FastJson();
+	}
+	
 	public static FastJsonFactory me() {
 		return me;
 	}
