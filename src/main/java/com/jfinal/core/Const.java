5649336bacc77a721c48773e48450cfcfab8c4b1
James <jfinal@126.com>
2022-04-10 22:09:14 +0800

    jfinal 4.9.23 release ^_^

--- src/main/java/com/jfinal/core/Const.java
+++ src/main/java/com/jfinal/core/Const.java
@@ -23,7 +23,7 @@ import com.jfinal.render.ViewType;
  */
 public interface Const {
 	
-	String JFINAL_VERSION = "4.9.22";
+	String JFINAL_VERSION = "4.9.23";
 	
 	ViewType DEFAULT_VIEW_TYPE = ViewType.JFINAL_TEMPLATE;
 	
