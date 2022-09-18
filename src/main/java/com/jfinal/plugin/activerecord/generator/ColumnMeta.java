4f9cb6e08c58ffbaf65b0e3809dac46a202881f2
James <jfinal@126.com>
2022-09-18 23:17:52 +0800

    添加获取字段自增属性，便于用户扩展新功能

--- src/main/java/com/jfinal/plugin/activerecord/generator/ColumnMeta.java
+++ src/main/java/com/jfinal/plugin/activerecord/generator/ColumnMeta.java
@@ -23,23 +23,25 @@ import java.io.Serializable;
  */
 @SuppressWarnings("serial")
 public class ColumnMeta implements Serializable  {
-	
+
 	public String name;				// 字段名
 	public String javaType;			// 字段对应的 java 类型
 	public String attrName;			// 字段对应的属性名
-	
+
 	// ---------
-	
+
 	/*
 	-----------+---------+------+-----+---------+----------------
 	 Field     | Type    | Null | Key | Default | Remarks
 	-----------+---------+------+-----+---------+----------------
-	 id		   | int(11) | NO	| PRI | NULL	| remarks here	
+	 id		   | int(11) | NO	| PRI | NULL	| remarks here
 	*/
 	public String type;				// 字段类型(附带字段长度与小数点)，例如：decimal(11,2)
 	public String isNullable;		// 是否允许空值
 	public String isPrimaryKey;		// 是否主键
 	public String defaultValue;		// 默认值
 	public String remarks;			// 字段备注
+
+	public Boolean isAutoIncrement;	// 是否自增
 }
 
