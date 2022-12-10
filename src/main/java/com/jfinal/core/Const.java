262502d55f2827b2790d809e34d8d7cda4ef1ede
James <jfinal@126.com>
2022-12-11 01:59:23 +0800

    jfinal 5.0.8 release ^_^

--- src/main/java/com/jfinal/core/Const.java
+++ src/main/java/com/jfinal/core/Const.java
@@ -23,7 +23,7 @@ import com.jfinal.render.ViewType;
  */
 public interface Const {
 
-	String JFINAL_VERSION = "5.0.7";
+	String JFINAL_VERSION = "5.0.8";
 
 	ViewType DEFAULT_VIEW_TYPE = ViewType.JFINAL_TEMPLATE;
 
