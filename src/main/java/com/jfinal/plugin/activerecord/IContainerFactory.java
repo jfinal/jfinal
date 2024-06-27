d2916f4050f9fd079c959490fc40b03954174ba4
James <jfinal@126.com>
2024-06-27 10:11:25 +0800

    IContainerFactory 支持序列化

--- src/main/java/com/jfinal/plugin/activerecord/IContainerFactory.java
+++ src/main/java/com/jfinal/plugin/activerecord/IContainerFactory.java
@@ -16,29 +16,33 @@
 
 package com.jfinal.plugin.activerecord;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 @SuppressWarnings("rawtypes")
-public interface IContainerFactory {
+public interface IContainerFactory extends Serializable {
+
 	Map getAttrsMap();
 	Map getColumnsMap();
 	Set getModifyFlagSet();
-	
+
 	static final IContainerFactory defaultContainerFactory = new IContainerFactory() {
-		
+
 		public Map<String, Object> getAttrsMap() {
 			return new HashMap<String, Object>();
 		}
-		
+
 		public Map<String, Object> getColumnsMap() {
 			return new HashMap<String, Object>();
 		}
-		
+
 		public Set<String> getModifyFlagSet() {
 			return new HashSet<String>();
 		}
 	};
 }
+
+
