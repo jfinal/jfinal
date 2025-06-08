f69c67d666c6e0a8a68e5ad252cc1a70193df2b6
zhanbo <jfinal@126.com>
2025-06-08 23:17:16 +0800

    jfinal 5.2.6

--- src/main/java/com/jfinal/template/stat/ast/SetGlobal.java
+++ src/main/java/com/jfinal/template/stat/ast/SetGlobal.java
@@ -27,28 +27,28 @@ import com.jfinal.template.stat.Scope;
 
 /**
  * SetGlobal 设置全局变量，全局作用域是指本次请求的整个 template
- * 
+ *
  * 适用于极少数的在内层作用域中希望直接操作顶层作用域的场景
  */
 public class SetGlobal  extends Stat {
-    
+
     private Expr expr;
-    
+
     public SetGlobal(ExprList exprList, Location location) {
         if (exprList.length() == 0) {
             throw new ParseException("The parameter of #setGlobal directive can not be blank", location);
         }
-        
+
         /* 放开对表达式类型的限定
         for (Expr expr : exprList.getExprArray()) {
             if ( !(expr instanceof Assign || expr instanceof IncDec) ) {
                 throw new ParseException("#setGlobal directive only supports assignment expressions", location);
             }
         }*/
-        
+
         this.expr = exprList.getActualExpr();
     }
-    
+
     public void exec(Env env, Scope scope, Writer writer) {
         Ctrl ctrl = scope.getCtrl();
         try {
