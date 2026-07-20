f547e2f7e73268280e1742aefb7bb32987ce1902
zhanbo <jfinal@126.com>
2026-07-20 22:15:08 +0800

    移除 toDate(LocalTime localTime) 方法，不支持 LocalTime 转 Date

--- src/main/java/com/jfinal/kit/TimeKit.java
+++ src/main/java/com/jfinal/kit/TimeKit.java
@@ -265,16 +265,16 @@ public class TimeKit {
         return Date.from(instant);
     }
 
-    /**
-     * java.time.LocalTime --> java.util.Date
-     */
-    public static Date toDate(LocalTime localTime) {
-        LocalDate localDate = LocalDate.now();
-        LocalDateTime localDateTime = LocalDateTime.of(localDate, localTime);
-        ZoneId zone = ZoneId.systemDefault();
-        Instant instant = localDateTime.atZone(zone).toInstant();
-        return Date.from(instant);
-    }
+    // /**
+    //  * java.time.LocalTime --> java.util.Date
+    //  */
+    // public static Date toDate(LocalTime localTime) {
+    //     LocalDate localDate = LocalDate.now();
+    //     LocalDateTime localDateTime = LocalDateTime.of(localDate, localTime);
+    //     ZoneId zone = ZoneId.systemDefault();
+    //     Instant instant = localDateTime.atZone(zone).toInstant();
+    //     return Date.from(instant);
+    // }
 
     /**
      * java.time.LocalTime --> java.util.Date
