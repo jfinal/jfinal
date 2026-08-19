4c5246731b7e3054dc27d22adbd311c4a41f7f45
zhanbo <jfinal@126.com>
2026-08-19 22:20:42 +0800

    修改注释

--- src/main/java/com/jfinal/plugin/activerecord/generator/BaseModelGenerator.java
+++ src/main/java/com/jfinal/plugin/activerecord/generator/BaseModelGenerator.java
@@ -58,11 +58,11 @@ public class BaseModelGenerator {
 		put("java.lang.Short", "getShort");
 		put("java.lang.Byte", "getByte");
 
-		// 新增两种可自动转换类型的 getter 方法
+		// 日期时间类型使用转换 getter，使生成类型不必与 JDBC 运行时对象完全相同
 		put("java.util.Date", "getDate");
-		put("java.time.LocalDateTime", "getLocalDateTime");
 		put("java.time.LocalDate", "getLocalDate");
 		put("java.time.LocalTime", "getLocalTime");
+		put("java.time.LocalDateTime", "getLocalDateTime");
 
 		// 新增 TypeKit 转换类之后，支持了更多的类型
 		put("java.lang.Boolean", "getBoolean");
